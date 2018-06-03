18f263049e288740d70dae49082b7256bd43c05a
Liam <lgoo0003@student.monash.edu>
2018-06-03 14:51:47 +1000

    added calculation algorithm

--- DiscountCalculator/app/src/main/java/au/com/scroogetech/discountcalculator/MainActivity.java
+++ DiscountCalculator/app/src/main/java/au/com/scroogetech/discountcalculator/MainActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //calculation variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public double calcDiscount(int cost, int discount){
+        double total=0;
+
+        total = cost - (cost * discount);
+
+        return total;
     }
 }
