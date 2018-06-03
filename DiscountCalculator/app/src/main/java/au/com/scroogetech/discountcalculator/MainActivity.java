0ccb08d6dd0679b26b1e2aed22e2549ab5da8223
Liam <lgoo0003@student.monash.edu>
2018-06-03 15:14:13 +1000

    added button implementation

--- DiscountCalculator/app/src/main/java/au/com/scroogetech/discountcalculator/MainActivity.java
+++ DiscountCalculator/app/src/main/java/au/com/scroogetech/discountcalculator/MainActivity.java
@@ -2,6 +2,10 @@ package au.com.scroogetech.discountcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     //calculation variables
@@ -10,9 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get button
+        final Button calcButton = (Button) findViewById(R.id.calculateButton);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get other values
+                EditText costText = (EditText) findViewById(R.id.costText);
+                EditText discountText = (EditText) findViewById(R.id.discountText);
+
+                double cost = Double.parseDouble(costText.toString());
+                double discountVal = Double.parseDouble(discountText.toString());
+
+
+                double total = calcDiscount(cost,discountVal);
+
+                TextView totalView = (TextView) findViewById(R.id.totalTextView);
+                String totalDouble = Double.toString(total);
+                totalView.setText(totalDouble);
+
+
+            }
+        });
     }
 
-    public double calcDiscount(int cost, int discount){
+    public double calcDiscount(double cost, double discount){
         double total=0;
 
         total = cost - (cost * discount);
